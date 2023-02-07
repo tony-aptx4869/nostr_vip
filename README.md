e9b35f2a85ae4e37f6ef3bbc254b17323ff6df65
Tony Chang <tonychang7869@gmail.com>
2023-02-07 23:29:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Nostr VIP
+
+You can get a free NIP-05 verified mark on Nostr (Damus, Amethyst, etc.) right now!
+
+Follow me on Nostr:
+
+```
+npub1vj42wvvf47q5jallthkmh2cz9hcrpuwhmulxxpavav0amvcdlprsds03ln
+```
+
---
+++ main.ts
@@ -0,0 +1,19 @@
+import { nip19 } from "nostr-tools";
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const { pathname, searchParams } = new URL(request.url);
+    const name = searchParams.get("name")!;
+    if (
+      pathname === "/.well-known/nostr.json" &&
+      typeof name === "string" &&
+      name.startsWith("npub")
+    ) {
+      return new Response(
+        JSON.stringify({ names: { [name]: nip19.decode(name).data } }),
+        { headers: { "cache-control": "public, max-age=31536000, immutable" } }
+      );
+    }
+    return new Response(null, { status: 404 });
+  },
+};
